744dd9141c1466b894f2164ba245fa14e6289dcd
mverdinn <mverdin@ucol.mx>
2024-05-23 11:09:54 -0600

    Correction details

--- Accesibilidad pagEstatica/exam.js
+++ Accesibilidad pagEstatica/exam.js
@@ -14,7 +14,7 @@ function mostrarUsuarios() {
       elementoLi.textContent = "Nombre: " + users[i].nombre + " - " + users[i].edad + " años" + " - Posición: " + users[i].pos;
       
       // Agregar datos de checkbox
-      elementoLi.textContent += " - Complementos: " + users[i].complementos.join(", ");
+      //elementoLi.textContent += " - Complementos: " + users[i].complementos.join(", ");
       // Agregar datos de radiobutton
       elementoLi.textContent += " - Estadio: " + users[i].estadio;
       // Agregar datos del select
@@ -41,20 +41,20 @@ function displayUsers() {
         // Inserta una celda para la posición del usuario en la fila creada
         const cellPos = row.insertCell(2);
         // Inserta una celda para los complementos del usuario en la fila creada
-        const cellComplementos = row.insertCell(3);
+        //const cellComplementos = row.insertCell(3);
         // Inserta una celda para el estadio del usuario en la fila creada
-        const cellEstadio = row.insertCell(4);
+        const cellEstadio = row.insertCell(3);
         // Inserta una celda para el equipo del usuario en la fila creada
-        const cellEquipo = row.insertCell(5);
+        const cellEquipo = row.insertCell(4);
         // Inserta una celda para los botones de acciones en la fila creada
-        const cellActions = row.insertCell(6);
+        const cellActions = row.insertCell(5);
 
         // Asigna el nombre, edad y posición del usuario a la celda correspondiente
         cellNombre.textContent = user.nombre;
         cellEdad.textContent = user.edad;
         cellPos.textContent = user.pos;
         // Muestra los complementos como una lista separada por comas
-        cellComplementos.textContent = user.complementos.join(", ");
+        //cellComplementos.textContent = user.complementos.join(", ");
         // Asigna el estadio del usuario a la celda correspondiente
         cellEstadio.textContent = user.estadio;
         // Asigna el equipo del usuario a la celda correspondiente
@@ -94,18 +94,19 @@ document.getElementById('addForm').addEventListener('submit', function(event) {
         const pos = document.getElementById('pos').value;
         // Capturar los valores de los checkbox
         const equipo = document.getElementById('equipo').value;
-        const salsaVerde = document.getElementById('Nike').checked ? "Nike" : "";
-        const salsaRoja = document.getElementById('Adidas').checked ? "Adidas" : "";
+        //const salsaVerde = document.getElementById('Nike').checked ? "Nike" : "";
+        //const salsaRoja = document.getElementById('Adidas').checked ? "Adidas" : "";
         // Capturar el valor del radio button del estadio seleccionado
         const estadioSeleccionado = document.querySelector('input[name="estadios"]:checked');
         const estadio = estadioSeleccionado ? estadioSeleccionado.value : ""; // Obtener el valor del estadio o un string vacío si no se seleccionó ninguno
         // Almacenar los complementos en un array
         const complementos = [];
-        if (salsaVerde !== "") complementos.push(salsaVerde);
-        if (salsaRoja !== "") complementos.push(salsaRoja);
+        //if (salsaVerde !== "") complementos.push(salsaVerde);
+        //if (salsaRoja !== "") complementos.push(salsaRoja);
         // Agregar el nuevo usuario al array users 
         // Se crea un nuevo objeto que representa al usuario utilizando los valores obtenidos del formulario.
-        users.push({ nombre, edad, pos, complementos, estadio, equipo });
+        //users.push({ nombre, edad, pos, complementos, estadio, equipo });
+        users.push({ nombre, edad, pos, estadio, equipo });
         displayUsers();
         mostrarUsuarios();
         this.reset(); // Limpiar el formulario después de agregar
@@ -137,8 +138,8 @@ function editUser(index) {
     document.getElementById('edad').value = user.edad;
     document.getElementById('pos').value = user.pos;
     // Checkboxs
-    document.getElementById('Nike').checked = user.complementos.includes('Nike');
-    document.getElementById('Adidas').checked = user.complementos.includes('Adidas');
+    //document.getElementById('Nike').checked = user.complementos.includes('Nike');
+    //document.getElementById('Adidas').checked = user.complementos.includes('Adidas');
     // Radiobuttons
     document.getElementById('estadioOlimpico').checked = user.estadio === 'Estadio Olímpico';
     document.getElementById('estadioSanJorge').checked = user.estadio === 'Estadio San Jorge';
@@ -158,8 +159,8 @@ document.getElementById('guardarCambios').addEventListener('click', function() {
     const newEdad = document.getElementById('edad').value;
     const newPos = document.getElementById('pos').value;
     // Obtener el valor de los checkboxs
-    const salsaVerde = document.getElementById('Nike').checked ? "Nike" : "";
-    const salsaRoja = document.getElementById('Adidas').checked ? "Adidas" : "";
+    //const salsaVerde = document.getElementById('Nike').checked ? "Nike" : "";
+    //const salsaRoja = document.getElementById('Adidas').checked ? "Adidas" : "";
     // Obtener el valor de los radiobuttons
     let estadioValue;
     const estadios = document.getElementsByName('estadios');
@@ -176,9 +177,9 @@ document.getElementById('guardarCambios').addEventListener('click', function() {
         users[editIndex].edad = newEdad;
         users[editIndex].pos = newPos;
         // Actualizar los valores de los checkboxs
-        users[editIndex].complementos = [];
-        if (salsaVerde !== "") users[editIndex].complementos.push(salsaVerde);
-        if (salsaRoja !== "") users[editIndex].complementos.push(salsaRoja);
+        //users[editIndex].complementos = [];
+        //if (salsaVerde !== "") users[editIndex].complementos.push(salsaVerde);
+        //if (salsaRoja !== "") users[editIndex].complementos.push(salsaRoja);
         // Actualizar el valor del radiobutton
         users[editIndex].estadio = estadioValue;
         // Actualizar el valor del select
